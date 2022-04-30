0725f32a253286b6467067c82515d0ba79fe455b
kang suhyeon <kzoen0040@naver.com>
2022-04-30 21:17:57 +0900

    [완성] driving.mp4 기준
    사다리꼴 마스크 2개 이용, hsv 필터, 가우시안블러, 케니에지, 허프라인 적용
    
    => 나중에 여기서 frame(원본영상)과 합치면 보기 더 좋을듯?

--- usingMsk.cpp
+++ usingMsk.cpp
@@ -14,9 +14,9 @@ using namespace std;
 
 Mat frame, frameOut;
 Mat gray, Gblur, edges;
-Mat msk, roi_msk, roi;
+Mat msk, roi_msk, roi_msk1, roi_msk2, roi1, roi2, roi;
 Mat hsv;
-Mat dst;   //hough
+Mat dst, cdst;   //hough
 vector<Vec4i> lines;    //hough
 int kernel_size=5;
 Mat yellow_msk, white_msk;
@@ -36,33 +36,57 @@ int main(int ac, char** av)
             if (frame.empty())
                 break;
     /****************관심영역 설정****************/
-        roi_msk = Mat::zeros(frame.size(), CV_8UC3);
-        Point points[1][4];
-        points[0][0] = Point(frame.cols/3-80,frame.rows/3);
-        points[0][1] = Point(50,frame.rows-300);
-        points[0][2] = Point(frame.cols-80,frame.rows-300);
-        points[0][3] = Point(frame.cols/3*2+80,frame.rows/3);
-        const Point* ppt[1] = {points[0]};
-        int npt[] = {4};
-        fillPoly(roi_msk, ppt, npt, 1, Scalar(255,255,255), 8);   //mask 제작
-        roi=frame.clone();  //roi=edges.clone();
-        bitwise_and(frame,roi_msk,roi); //bitwise_and(edges,msk,roi);
+        roi_msk1 = Mat::zeros(frame.size(), CV_8UC3);
+        Point points1[1][4];
+        points1[0][0] = Point(frame.cols/3+80,frame.rows/4+10);
+        points1[0][1] = Point(80,frame.rows-300);
+        points1[0][2] = Point(300,frame.rows-300);
+        points1[0][3] = Point(frame.cols/3+160,frame.rows/4+10);
+        const Point* ppt1[1] = {points1[0]};
+        int npt1[] = {4};
+        fillPoly(roi_msk1, ppt1, npt1, 1, Scalar(255,255,255), 8);   //mask1 제작
+
+        roi_msk2 = Mat::zeros(frame.size(), CV_8UC3);
+        Point points2[1][4];
+        points2[0][0] = Point(frame.cols/3*2-250,frame.rows/4+10);
+        points2[0][1] = Point(frame.cols-360,frame.rows-300);
+        points2[0][2] = Point(frame.cols-200,frame.rows-300);
+        points2[0][3] = Point(frame.cols/3*2-200,frame.rows/4+10);
+        const Point* ppt2[1] = {points2[0]};
+        int npt2[] = {4};
+        fillPoly(roi_msk2, ppt2, npt2, 1, Scalar(255,255,255), 8);   //mask2 제작
+       
+        roi1=frame.clone();  //roi=edges.clone();
+        bitwise_and(frame,roi_msk1,roi1);
+        roi2=frame.clone();
+        bitwise_and(frame,roi_msk2,roi2); //bitwise_and(edges,msk,roi);
+        bitwise_or(roi_msk1,roi_msk2,roi_msk);
+        bitwise_and(frame,roi_msk,roi);
     /********************HSV*********************/ 
         cvtColor(roi,hsv,COLOR_BGR2HSV);
-        inRange(hsv,Scalar(0,0,200),Scalar(180,255,255),white_msk);
-        bitwise_and(roi,roi,hsv,white_msk);
+        inRange(hsv,Scalar(22,50,200),Scalar(180,255,255),yellow_msk);
+        inRange(hsv,Scalar(0,0,200),Scalar(180,10,255),white_msk);
+        addWeighted(yellow_msk,1.0,white_msk,1.0,0.0,msk);
+        bitwise_and(roi,roi,hsv,msk);
+        Mat kernel = (Mat_<uchar>(3,3) << 1,1,1,
+                                          1,1,1,
+                                          1,1,1);
+        morphologyEx(msk,msk,MORPH_OPEN,kernel);
     /*****************블러, 에지******************/
-        GaussianBlur(hsv,Gblur,Size(5,5),0,0);
+        //blur(white_msk,Gblur,Size(5,5),Point(-1,-1));
+        GaussianBlur(msk,Gblur,Size(5,5),0,0);
+        //bilateralFilter(white_msk,Gblur,9,75,75,4);
+        //medianBlur(white_msk,Gblur,5);
         Canny(Gblur,edges,300,800,kernel_size);//Canny(Gblur,edges,100,600,kernel_size);
     /*******************Lines********************/
-        cvtColor(edges,dst,COLOR_GRAY2BGR);
+        cvtColor(edges,cdst,COLOR_GRAY2BGR);
         HoughLinesP(edges, lines, 1, CV_PI / 180, 50, 100, 20);//hough
                 for (size_t i = 0; i < lines.size(); i++) {//hough
                     Vec4i l = lines[i];//hough
-                    line(dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, 4);//hough
+                    line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, 4);//hough
                 }
     /*****************영상보기********************/
-        imshow(window_name, dst);
+        imshow(window_name, cdst);
     /****************영상 나가기******************/
         char key = (char)waitKey(30);
         switch (key) {
