b30b4215199feff3adf63f85cd1959a1d6ce16a1
kang suhyeon <kzoen0040@naver.com>
2022-04-30 18:40:30 +0900

    [미완] 일부 수정

--- usingMsk.cpp
+++ usingMsk.cpp
@@ -14,10 +14,12 @@ using namespace std;
 
 Mat frame, frameOut;
 Mat gray, Gblur, edges;
-Mat msk, roi;
-Mat dst,cdst;   //hough
+Mat msk, roi_msk, roi;
+Mat hsv;
+Mat dst;   //hough
 vector<Vec4i> lines;    //hough
 int kernel_size=5;
+Mat yellow_msk, white_msk;
 
 int main(int ac, char** av)
 {
@@ -33,32 +35,34 @@ int main(int ac, char** av)
             capture >> frame;
             if (frame.empty())
                 break;
-    /*****************블러, 에지******************/
-        cvtColor(frame,gray,COLOR_BGR2GRAY);    //영상 흑백처리
-        GaussianBlur(gray,Gblur,Size(5,5),0,0);
-        Canny(Gblur,edges,200,600,kernel_size);
     /****************관심영역 설정****************/
-        msk = Mat::zeros(frame.size(), CV_8UC1);
+        roi_msk = Mat::zeros(frame.size(), CV_8UC3);
         Point points[1][4];
-        points[0][0] = Point(edges.cols/3,edges.rows/4*2.5);
-        points[0][1] = Point(100,edges.rows-50);
-        points[0][2] = Point(edges.cols-100,edges.rows-50);
-        points[0][3] = Point(edges.cols/3*2,edges.rows/4*2.5);
+        points[0][0] = Point(frame.cols/3-80,frame.rows/3);
+        points[0][1] = Point(50,frame.rows-300);
+        points[0][2] = Point(frame.cols-80,frame.rows-300);
+        points[0][3] = Point(frame.cols/3*2+80,frame.rows/3);
         const Point* ppt[1] = {points[0]};
         int npt[] = {4};
-        fillPoly(msk, ppt, npt, 1, Scalar(255,255,255), 8);   //mask 제작
-        roi=edges.clone();
-        bitwise_and(edges,msk,roi);
+        fillPoly(roi_msk, ppt, npt, 1, Scalar(255,255,255), 8);   //mask 제작
+        roi=frame.clone();  //roi=edges.clone();
+        bitwise_and(frame,roi_msk,roi); //bitwise_and(edges,msk,roi);
+    /********************HSV*********************/ 
+        cvtColor(roi,hsv,COLOR_BGR2HSV);
+        inRange(hsv,Scalar(0,0,200),Scalar(180,255,255),white_msk);
+        bitwise_and(roi,roi,hsv,white_msk);
+    /*****************블러, 에지******************/
+        GaussianBlur(hsv,Gblur,Size(5,5),0,0);
+        Canny(Gblur,edges,300,800,kernel_size);//Canny(Gblur,edges,100,600,kernel_size);
     /*******************Lines********************/
-        //line(roi,)
-        cvtColor(roi,cdst,COLOR_GRAY2BGR);
-        HoughLinesP(roi, lines, 1, CV_PI / 180, 50, 100, 20);//hough
+        cvtColor(edges,dst,COLOR_GRAY2BGR);
+        HoughLinesP(edges, lines, 1, CV_PI / 180, 50, 100, 20);//hough
                 for (size_t i = 0; i < lines.size(); i++) {//hough
                     Vec4i l = lines[i];//hough
-                    line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, 4);//hough
+                    line(dst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, 4);//hough
                 }
     /*****************영상보기********************/
-        imshow(window_name,cdst);
+        imshow(window_name, dst);
     /****************영상 나가기******************/
         char key = (char)waitKey(30);
         switch (key) {
