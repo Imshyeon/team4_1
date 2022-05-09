3ac9cb13fb466c0c0c95b947b99d63b356e1e06a
joomong0126 <77429760+joomong0126@users.noreply.github.com>
2022-05-09 18:50:07 +0900

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,2 +1,3 @@
 // 제세한 설명: https://cobit-git.github.io/deepThinkCar_doc/step_2
   //명령어: sudo git clone https://github.com/cobit-git/deepThinkCar.git
+// 차선인식, 사물인식(텐서플로우), 라즈베리캠, 자율주행 코드가 들어있음--> 그 대신 파이썬임.
